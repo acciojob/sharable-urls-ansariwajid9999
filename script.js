a250ea9dbee37a10c939429721f664bcaf5c415c
wajid ansari <112863100+ansariwajid9999@users.noreply.github.com>
2024-10-14 16:44:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// your code here
+const mySubmit = document.getElementById("mySubmit")
+const Name = document.getElementById("Name")
+const year = document.getElementById("year")
+const myH3 = document.getElementById("myH3")
+const reset = myH3.textContent
+
+mySubmit.onclick = () => {
+    myH3.textContent = reset
+    if(Name.value !== "" && year.value !== ""){
+        myH3.textContent += `?name=${Name.value}&year=${year.value}`
+    }
+    else if(Name.value !== ""){
+        myH3.textContent += `?name=${Name.value}`
+    }
+    else if(year.value !== ""){
+        myH3.textContent += `?year=${year.value}`
+    } 
+}
